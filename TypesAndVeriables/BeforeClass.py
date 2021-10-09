977982ba4ef658f73dad340f1fc5d2694e1d2e8a
GalaxSKL <s221399@student.uek.krakow.pl>
2021-10-09 17:28:24 +0200

    enter

---
+++ TypesAndVeriables/BeforeClass.py
@@ -0,0 +1,32 @@
+int(13)  # Int . #For bigger nubers doublelong.
+float(6.7)  # Float
+str("Hi!")  # String
+bool(True)  # Bool
+bool(False)  # Bool
+complex(1j)  # complex
+
+x = 6
+y = "Hello"
+print(x)
+print(y)
+print(type(x))
+print(type(y))
+"""
++ Additon x+y .ex 3+5 = 8
+- Subtraction x-y .ex 7-4 = 3
+* Multiplication x*y .ex 3*6 = 18
+/ Division x/y .ex 28/7 = 4
+% Modulus x % y .ex 20%6 = 2
+** Exponentation x**y .ex 2**5 = 32
+// Floor divisoon x//y .ex 15//2 = 7 #return int as result of division wichout the remnant 
+### mass string can be used as comments in python.
+"""
+"""
+# Bool Operators
+== Equal .ex x == y 
+!= Not equal .ex  x != y
+> Grather than .ex x > y
+< Less than .ex x < y
+>= Greather than or equal to .ex x >=y
+<= Less than or equal to x <= y
+"""
