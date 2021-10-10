2af07ae57afd261e1e5c1d476f1c1390d5b7fd6d
GalaxSKL <s221399@student.uek.krakow.pl>
2021-10-10 14:38:13 +0200

    enter

---
+++ TypesAndVeriables/task18.py
@@ -0,0 +1,6 @@
+print("How tall are u in centimeters")
+cm = int(input())
+foot = cm / 30.48
+foot_1 = int(foot)
+foot_2 = (foot-foot_1)*10
+print("I am {} cm tall, i.e. {} feet and {:.0f} inches.".format(cm, foot_1, foot_2))
