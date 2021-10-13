409aeeac201b957cdd4097c1810de9f9110600f0
Galax <s221399@student.uek.krakow.pl>
2021-10-13 18:23:17 +0200

    enter

--- task20.py
+++
@@ -1,6 +0,0 @@
-print("Enter your height in cm")
-height = int(input())
-print("Enter your mass in kg")
-mass = int(input())
-bmi = mass / height /height * 10000
-print("Your bmi is equal to {:.2f}".format(bmi))
