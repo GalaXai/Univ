c1904ae17601169837beb59d9045ccc3a5a41d54
GalaxSKL <92019780+GalaxSKL@users.noreply.github.com>
2021-10-13 13:00:00 +0200

    Add files via upload

---
+++ task20.py
@@ -0,0 +1,6 @@
+print("Enter your height in cm")
+height = int(input())
+print("Enter your mass in kg")
+mass = int(input())
+bmi = mass / height /height * 10000
+print("Your bmi is equal to {:.2f}".format(bmi))
---
+++ task23.py
@@ -0,0 +1,6 @@
+print("Type ammount")
+ammount = float(input())
+vat = 77
+vat23 = ammount*0.23
+print("Ammount : {}".format(ammount))
+print("VAT 23% : {}".format(vat23))
