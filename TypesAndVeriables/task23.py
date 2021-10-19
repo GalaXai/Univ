8a311fe844f78c893b500dcedfe0179c7671d6ed
Galax <s221399@student.uek.krakow.pl>
2021-10-19 20:07:01 +0200

    enter

--- TypesAndVeriables/task23.py
+++ TypesAndVeriables/task23.py
@@ -2,8 +2,8 @@
 vat = 0.77
 print("Type in the price")
 ammount = float(input())
-price_wVat = ammount * vat
-costOfTheVat = ammount * (1-vat)
+price_wVat = ammount / 1.23
+costOfTheVat = ammount - price_wVat
 print("Ammount {}".format(ammount))
 print("VAT 23% {:.2f}".format(costOfTheVat))
 print("Ammount wichout vat {:.2f}".format(price_wVat))
