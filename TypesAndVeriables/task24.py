f78cd68f176c17655ca4fd0307a8d346681e79a4
Galax <s221399@student.uek.krakow.pl>
2021-10-19 19:21:58 +0200

    enter

--- TypesAndVeriables/task24.py
+++ TypesAndVeriables/task24.py
@@ -1,6 +1,7 @@
 # VAT = 23%
 vat = 0.77
-ammount = 15.84
+print("Type in the price")
+ammount = float(input())
 price_wVat = ammount * vat
 costOfTheVat = ammount * (1-vat)
 print("Ammount {}".format(ammount))
