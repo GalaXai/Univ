418d691ade7d92e004515567e3865fa616a8e45a
GalaxSKL <92019780+GalaxSKL@users.noreply.github.com>
2021-10-20 12:55:17 +0200

    Update task15.py

--- task15.py
+++ task15.py
@@ -1,5 +1,17 @@
-#import mymath.py
+import random
 def read_number():
     guess = int(input("Enter your number: "))
     return guess
-print(read_number())
+def generate_number():
+    answer = random.randrange(1,9)
+    return answer
+guess = 0
+answer = generate_number()
+attempts = 3
+while guess!=answer and attempts > 0:
+    guess = read_number()
+    attempts -=1
+if guess == answer :
+    print("congratulation")
+else:
+    print(" The number was equal to {}".format(answer))
