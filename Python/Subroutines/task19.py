dca6c03a460e6acabf1e4ac29eab7aa6abe0d41b
Galax <s221399@student.uek.krakow.pl>
2021-10-20 21:02:45 +0200

    enter

---
+++ Python/Subroutines/task19.py
@@ -0,0 +1,11 @@
+def numberInRange(number):
+    x = 3
+    y = 15
+    for i in range(x, y):
+        if i == number:
+            return print(True)
+    return print(False)
+
+
+numberInRange(155)
+# jak nie tak to idk co trzeba tu zrobic z polecanie
