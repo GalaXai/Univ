e05d3b5ce9e86abd52b1dd5fbf80f3551e9ad4f8
GalaxSKL <92019780+GalaxSKL@users.noreply.github.com>
2021-10-20 21:23:51 +0200

    Delete Python/test directory

--- Python/test/task19.py
+++
@@ -1,5 +0,0 @@
-a=3
-b=4
-c=5
-area = ((a+b+c)*(a+b-c)*(a-b+c)*(b+c-a))**(1/2)/4
-print ("Are of the triange is equal to {}".format(area))
--- Python/test/task21.py
+++
@@ -1,5 +0,0 @@
-import random
-x = random.randrange(1,6)
-y = random.randrange(1,6)
-c = random.randrange(1,6)
-print(x+y+c)
