e9ed30e2b1e5ba5546ce1ea77eab06b37455bef1
GalaxSKL <92019780+GalaxSKL@users.noreply.github.com>
2021-10-21 18:43:52 +0200

    Update task19.py

--- Python/Subroutines/task19.py
+++ Python/Subroutines/task19.py
@@ -1,11 +1,3 @@
-def numberInRange(number):
-    x = 3
-    y = 15
-    for i in range(x, y):
-        if i == number:
-            return print(True)
-    return print(False)
-
-
-numberInRange(155)
-# jak nie tak to idk co trzeba tu zrobic z polecanie
+def numberInRange(x,y,number):
+   return x <= number <= y
+print(numberInRange(1,8,5))
