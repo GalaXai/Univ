a6965c292e922c54d50d9b8fd261628c4795b67e
Galax <s221399@student.uek.krakow.pl>
2021-10-25 15:29:48 +0200

    enter

---
+++ Python/04-Arrays/task11.py
@@ -0,0 +1,12 @@
+def comparee(array1,array2):
+    if len(array1)==len(array2):
+        for x in range(len(array1)):
+            Flag = array1[x] == array2[x]
+    else:
+        Flag = False
+    return Flag
+
+print(comparee(["water","book","sky"],["water","book","sky"]))
+print(comparee([True,False],[True,False,True]))
+print(comparee([5,3,1],[5,3,1]))
+print(comparee([3,2,1],[3,2]))
