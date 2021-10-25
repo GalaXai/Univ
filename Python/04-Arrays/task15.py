772577f0a65e84780ec02d6bb71a9abe093ba4d5
Galax <s221399@student.uek.krakow.pl>
2021-10-25 16:43:44 +0200

    enter

--- Python/04-Arrays/task15.py
+++ Python/04-Arrays/task15.py
@@ -1,2 +1,6 @@
 colors = ["olive","yellow","navy","navy","silver","red","blue"]
 #text file work?
+file = open("task15.txt","w")
+for i in range(len(colors)):
+    file.write(colors[i]+"\n")
+file.close
