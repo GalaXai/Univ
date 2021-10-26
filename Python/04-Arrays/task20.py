f0dc786231ed2e6bf5acaa6d46da35d31c286c0c
Galax <s221399@student.uek.krakow.pl>
2021-10-26 21:45:56 +0200

    enter

---
+++ Python/04-Arrays/task20.py
@@ -0,0 +1,12 @@
+def occour(number,array):
+    flag = False
+    for num in array:
+        if number ==  num:
+            flag = True
+            break
+    if flag == True:
+        return print("Result: number {} appears in the array".format(number))
+    else:
+        return print("Result: number {}  doesn't appears in the array".format(number))
+numbers = [15,38,7,23,14]
+occour(23,numbers)
---
+++ Python/04-Arrays/task21.py
@@ -0,0 +1,10 @@
+def secondLargest(array):
+    array.sort()
+    for i in range (1,len(array)+1):
+        if array[-i-1] != array[-i]:
+            return array[-i-1]
+numbers = [5,1,9,6,1]
+print(secondLargest(numbers))
+#
+test = [1,3,9,9,9,9,9,7,9,9,9,9,9,9,9,9,9]
+print(secondLargest(test))
