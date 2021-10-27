cd167fd9f5bbf9500834550ecd30a560a9148095
GalaxSKL <92019780+GalaxSKL@users.noreply.github.com>
2021-10-27 12:55:12 +0200

    Update and rename task18(TD).py to task18.py

---
+++ Python/04-Arrays/task18.py
@@ -0,0 +1,9 @@
+def BubbleSort(array):
+    for i in array:
+        for j in range(len(array)-1):
+            if array[j] > array[j+1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
+   
+numbers = [4,3,5,1,2]
+print(BubbleSort(numbers)
