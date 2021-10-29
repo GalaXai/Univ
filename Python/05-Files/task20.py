8e08ae638781b608070988ef27865c2094093a90
Galax <s221399@student.uek.krakow.pl>
2021-10-29 11:04:34 +0200

    enter

---
+++ Python/05-Files/task20.py
@@ -0,0 +1,3 @@
+with open('Python/05-Files/numbers1-50.txt.','w') as file:
+    for i in range(1,50):
+        file.write(str(i)+"\n")
---
+++ Python/05-Files/task22.py
@@ -0,0 +1,3 @@
+with open('Python/05-Files/numbersPower.txt.','w') as file:
+    for i in range(1,10):
+        file.write(str(i**1)+','+str(i**2)+','+str(i**3)+'\n')
