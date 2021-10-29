824034d6f512ea448588479232576cfa33067673
Galax <s221399@student.uek.krakow.pl>
2021-10-29 23:24:51 +0200

    enter

--- Python/05-Files/task27.py
+++ Python/05-Files/task27.py
@@ -1,6 +1,8 @@
 import re
 lore = "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-words =re.findall("\w+",lore)
-#jebac to
-
-print(words)
+words = re.findall("\w+",lore)
+validate = []
+for word in words:
+    if len(word) >= 6:
+        validate.append(word)
+print(validate)
---
+++ Python/05-Files/task28.py
@@ -0,0 +1,8 @@
+import re
+with open('Python/05-Files/grades.txt.') as file:
+    text = file.read()
+    grades =  re.findall("\d[.]\d",text)
+    sumOfGrades = 0
+    for number in grades:
+        sumOfGrades += float(number)
+print("{:.2f}".format(sumOfGrades/len(grades)))
