84f34dd9e0f82b2538106386d21c26f859d77b70
Galax <s221399@student.uek.krakow.pl>
2021-11-02 20:14:36 +0100

    enter

---
+++ Python/06-DictionariesStacksAndQueues/task13.py
@@ -0,0 +1,7 @@
+import json
+import json
+
+with open("Python/06-DictionariesStacksAndQueues/imported.json" ,"r", encoding="utf-8") as file:
+    data = json.load(file)
+for key,value in data.items():
+    print(key,":",value)
