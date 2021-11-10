9bb0e21d972e3287eee84a868ea96148d5065125
Galax <92019780+GalaxUniv@users.noreply.github.com>
2021-11-10 12:26:14 +0100

    Update task06.py

--- Python/04-Arrays/task06.py
+++ Python/04-Arrays/task06.py
@@ -8,5 +8,6 @@ print(wynik/len(numbers))
 numbers = [15,8,31,47,2,19]
 wynik = 0
 for x in numbers:
-    wynik=+1
-print(sum(numbers)/len(numbers)
+    wynik+=x
+print(wynik/len(numbers))
+print(sum(numbers)/len(numbers))
