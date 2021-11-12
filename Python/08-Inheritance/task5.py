bc37daaf6503ab026800b19ed3fc05b42c6207eb
Galax <s221399@student.uek.krakow.pl>
2021-11-12 21:17:28 +0100

    enter

---
+++ Python/08-Inheritance/task5.py
@@ -0,0 +1,13 @@
+class music():
+    def __init__(self,artist,track_title,album,year):
+        self.artist = artist
+        self.track_title = track_title
+        self.album = album
+        self.year = year
+
+    def __str__(self):
+        return "Performer: "+self.artist+"\nSong: "+self.track_title+"\nAlbum: "+self.album+"\nYear: "+str(self.year)
+
+if __name__ == '__main__':
+    song0 = music("Ed Sheeran","Hearts Don't Break Around Here","Divide",2017)
+    print(song0)
