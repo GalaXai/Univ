d5dba3bf5b3a6597358f0d8ef5893f25b1cd2411
Galax <92019780+GalaxUniv@users.noreply.github.com>
2021-11-22 10:29:38 +0100

    Update task28(Recursive).py

--- Python/02-ControlStructures/task28(Recursive).py
+++ Python/02-ControlStructures/task28(Recursive).py
@@ -1,9 +1,8 @@
 #vvv print sum of fibonacci numbers
 def Fibonacci(n):
-    if n == 0 :
-        return 0
-    if n == 1 :
-        return 1
+    if n <= 1:
+       return n
     return (Fibonacci(n-1) + Fibonacci(n-2))
 
-print(Fibonacci(6))
+x=10
+print(Fibonacci(x-1))
