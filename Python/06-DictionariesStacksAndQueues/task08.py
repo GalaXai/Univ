3c61e12d5ea61aa7130719500d4ff3eb2ba9f3dd
Galax <GalaxCherryL@gmail.com>
2021-11-24 20:42:36 +0100

    Update task08.py

--- Python/06-DictionariesStacksAndQueues/task08.py
+++ Python/06-DictionariesStacksAndQueues/task08.py
@@ -2,7 +2,7 @@ person = {
     "name": "Stefan",
     "surname": "Banach",
     "age": 25,
-    "hobby": ["swimming","excursions"],
+    "hobby": {"swimming","excursions"},
     "married": True,
     "phone": {"landline":"123444321","mobile":"111333555"}
 }
@@ -12,7 +12,9 @@ print(person["hobby"])
 person["surname"] = "Nowak"
 person["married"] = False
 person["sex"] = "Male"
-person["hobby"].append("bicycle")
+person["hobby"].update("bicycle")
 person["phone"].update({"work":'313131444'})
 for key, value in person.items():
     print(key, "=",value)
+x = {"landline":"123444321","mobile":"111333555"}
+print(type(x))
