fd54e069bded2358004aaa34f87faf3ebfffb4d3
Galax <GalaxCherryL@gmail.com>
2021-11-27 18:23:37 +0100

    enter

--- Python/06-DictionariesStacksAndQueues/task20.py
+++ Python/06-DictionariesStacksAndQueues/task20.py
@@ -3,14 +3,14 @@ import json
 with open("06-DictionariesStacksAndQueues/students.json" ,"r") as file:
     with open("06-DictionariesStacksAndQueues/limited.json" ,"w") as f:
         data = json.load(file)
-        for i in range(len(data)):
-            temp = data[i]
-            dict(temp)
     #name surname and id
-            output = {
-                "name": temp.get("name "),
-                "surname": temp.get("surname"),
-                "student id": temp.get("student id"),
-            }
-            json.dump(output, f,indent=True)
-            f.write('\n')
+        output = {}
+        ans=[]
+        for i in range(len(data)):
+            output = {}
+            output["name "] =data[i]["name "]
+            output["surname"] =data[i]["surname"]
+            output["student id"] =data[i]["student id"]
+            ans.append(output)
+        json.dump(ans, f,indent=True)
+        f.write('\n')
