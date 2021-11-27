8ecbab3d5ad91b4c10916047be2c695f4e990567
Galax <GalaxCherryL@gmail.com>
2021-11-27 01:57:53 +0100

    enter

---
+++ EWD/05-Web Document Manipulation/date.js
@@ -0,0 +1,8 @@
+function date(){
+    var today = new Date();
+    var dd = String(today.getDate())
+    var mm = String(today.getMonth() + 1) //January is 0!
+    var yyyy = today.getFullYear();
+    today = dd + '/' + mm + '/' + yyyy;
+    window.alert(today)
+}
