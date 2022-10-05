326292567f57f890aafd00b4aeee8ece1b2e6ce1
Galax <GalaxCherryL@gmail.com>
2022-10-05 13:28:06 +0200

    Java class

---
+++ Java/01-CourseIntroduction/prep1_3.java
@@ -0,0 +1,7 @@
+public class prep1_3 {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        // dont be so rude :(
+        System.out.println("How are you?");
+    }
+}
