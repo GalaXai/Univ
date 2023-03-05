80ce68163ac71b658731d7e06508f5a3db614536
Galax <afterhoursbilly@gmail.com>
2023-03-05 13:07:34 +0100

    fixed typo

--- pp4/01_Assignment/courses.py
+++ pp4/01_Assignment/courses.py
@@ -1,10 +1,10 @@
 class Course:
     def __init__(self):
-        self.stundets_list = []
+        self.students = []
 
     def add_student(self,student):
-        if len(self.stundets_list) < 10:
-            self.stundets_list.append(student)
+        if len(self.students) < 10:
+            self.students.append(student)
             print(f"{student} has been enrolled in the course.")
         else:
             print("The course is already full. Enrollment is closed.")
