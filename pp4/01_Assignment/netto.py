f6342978d93dc2d9f57c9201478ab9d70365f271
Galax <afterhoursbilly@gmail.com>
2023-03-05 12:54:02 +0100

    fixed function

--- pp4/01_Assignment/netto.py
+++ pp4/01_Assignment/netto.py
@@ -1,2 +1,5 @@
 def calc_netto(brutto_ammount,tax):
-    return round(brutto_ammount/(1+tax/100),2)
+    if tax >=0:
+        return round(brutto_ammount/(1+(tax/100)),2)
+    else:
+        raise Exception("Tax value should be positive number")
